a6b57064bcb27d4889bf39be899abb7065c74429
Владимир <nickysheen@yandex.ru>
2020-01-09 22:20:29 +0100

    delete content

--- a/_projects/sakh-development-agency/js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('ol, ul').prev('p').css('margin-bottom', '0'); //lists captions
-});
-
-function handleError() {
-    return true;
-}
-window.onerror = handleError;
